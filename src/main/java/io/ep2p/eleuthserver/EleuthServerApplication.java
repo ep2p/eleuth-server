fc248c5a3e6eb1afb9b5841cac4c44bd44bbc999
Sepehr Ghorbanpoor <sepehrgh01@gmail.com>
2021-07-09 10:07:52 +0430

    init springboot

---
+++ src/main/java/io/ep2p/eleuthserver/EleuthServerApplication.java
@@ -0,0 +1,13 @@
+package io.ep2p.eleuthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleuthServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleuthServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/ep2p/eleuthserver/EleuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.ep2p.eleuthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleuthServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
