95699a1a3e482d5ff7aa0ebc43366ce4de47f501
Sepehr Ghorbanpoor <sepehrgh01@gmail.com>
2021-07-09 11:25:16 +0430

    feat: base websocket configuration

---
+++ src/main/java/io/ep2p/eleuthserver/config/WebsocketConfiguration.java
@@ -0,0 +1,9 @@
+package io.ep2p.eleuthserver.config;
+
+import io.ep2p.row.server.annotations.EnableRowPublisher;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableRowPublisher
+public class WebsocketConfiguration {
+}
