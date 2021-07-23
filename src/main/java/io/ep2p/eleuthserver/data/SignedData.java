1075e65abae00634c6736088ed5dc37b885f360a
sepehr-gh <sepehrgh01@gmail.com>
2021-07-23 12:19:20 +0430

    feat: add SignedData

---
+++ src/main/java/io/ep2p/eleuthserver/data/SignedData.java
@@ -0,0 +1,22 @@
+package io.ep2p.eleuthserver.data;
+
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigInteger;
+
+/**
+ * Data model wrapper with signature support
+ * @param <D> Serializable data object which is signed
+ */
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Setter
+public class SignedData<D extends Serializable> {
+    private D data;
+    private String signature;
+    private BigInteger key;
+    private String publicKey;
+}
