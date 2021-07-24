406894754dd675c42963a3a0296bd90c8a6baabc
sepehr-gh <sepehrgh01@gmail.com>
2021-07-24 13:59:46 +0430

    feat: repository extension

--- src/main/java/io/ep2p/eleuthserver/database/repository/ConversationMessageRepository.java
+++ src/main/java/io/ep2p/eleuthserver/database/repository/ConversationMessageRepository.java
@@ -1,12 +1,11 @@
 package io.ep2p.eleuthserver.database.repository;
 
+import io.ep2p.eleuthserver.data.SignedData;
+import io.ep2p.eleuthserver.data.message.ConversationMessage;
 import io.ep2p.eleuthserver.database.entity.ConversationMessageEntity;
 import io.ep2p.somnia.config.dynamic.DynamicRepository;
 import io.ep2p.somnia.storage.SomniaRepository;
 
-import java.io.Serializable;
-
-// TODO
 @DynamicRepository
-public interface ConversationMessageRepository extends SomniaRepository<Serializable, ConversationMessageEntity> {
+public interface ConversationMessageRepository extends SomniaRepository<SignedData<ConversationMessage>, ConversationMessageEntity> {
 }
--- src/main/java/io/ep2p/eleuthserver/database/repository/MessageDataRepository.java
+++ src/main/java/io/ep2p/eleuthserver/database/repository/MessageDataRepository.java
@@ -1,12 +1,11 @@
 package io.ep2p.eleuthserver.database.repository;
 
+import io.ep2p.eleuthserver.data.SignedData;
+import io.ep2p.eleuthserver.data.message.PayloadMessage;
 import io.ep2p.eleuthserver.database.entity.MessageDataEntity;
 import io.ep2p.somnia.config.dynamic.DynamicRepository;
 import io.ep2p.somnia.storage.SomniaRepository;
 
-import java.io.Serializable;
-
-// TODO
 @DynamicRepository
-public interface MessageDataRepository extends SomniaRepository<Serializable, MessageDataEntity> {
+public interface MessageDataRepository extends SomniaRepository<SignedData<PayloadMessage>, MessageDataEntity> {
 }
